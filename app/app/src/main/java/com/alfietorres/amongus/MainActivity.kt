573b4594fc2147c4e66f90d5f685197c80798c2f
Alfie Torres <alfielytorres@gmail.com>
2020-07-17 18:30:08 +1000

    added data bindings

---
+++ app/app/src/main/java/com/alfietorres/amongus/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.alfietorres.amongus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.alfietorres.amongus.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+       binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+    }
+}
